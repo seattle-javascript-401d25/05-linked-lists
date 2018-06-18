d1ed0c1a3796f4e0c067046e2f67753cb189ac1c
Jennifer <jlawrence010@gmail.com>
2018-06-17 19:59:08 -0700

    fixed erros in ll.test.js

--- src/__test__/ll.test.js
+++ src/__test__/ll.test.js
@@ -4,7 +4,7 @@ const LinkedList = require('../linked-list/ll');
 const Node = require('../linked-list/node');
 const inspect = require('./utils');
 
-describe('testing linked list'), () => {
+describe('testing linked list', () => {
   let testList; 
   beforeEach(() => {
     testList = new LinkedList();
@@ -14,7 +14,7 @@ describe('testing linked list'), () => {
     testList = null;
   });
 
-  test('making sure we instantiate a new instance', () => {
+  test('to create a new instance', () => {
     expect(testList.head).toBeNull();
   });
 
@@ -35,22 +35,22 @@ describe('testing linked list'), () => {
   });
 
   test('#insertAtEnd', () => {
-      testList.insertAtEnd(5);
-      expect(testList.head.value).toEqual(5);
+    testList.insertAtEnd(5);
+    expect(testList.head.value).toEqual(5);
 
-      testList.insertAtEnd(6);
-      expect(testList.head.value).toEqual(5);
-      expect(testList.head.next.value).toEqual(6);
-      expect(testList.head.next.next.value).toEqual(7);
-      inspect(testList, 'INSERT AT END');
+    testList.insertAtEnd(6);
+    expect(testList.head.value).toEqual(5);
+    expect(testList.head.next.value).toEqual(6);
+    expect(testList.head.next.next.value).toEqual(7);
+    inspect(testList, 'INSERT AT END');
   });
 
   test('#find', () => {
-      testList.insertAtEnd(5);
-      testList.insertAtEnd(6);
-      testList.insertAtEnd(7);
+    testList.insertAtEnd(5);
+    testList.insertAtEnd(6);
+    testList.insertAtEnd(7);
 
-      expect(testList.find(5)).toBeInstanceOf(Node);
-      expect(testList.find(9)).toBeNull();
+    expect(testList.find(5)).toBeInstanceOf(Node);
+    expect(testList.find(9)).toBeNull();
   });
 });
